35bad4148a3871531d4a01f31e37ccc9e45c0b0d
Ennysonk12 <137646077+Ennysonk12@users.noreply.github.com>
2023-07-04 19:19:20 -0300

    Lista04 Questão 01.cpp

---
+++ Lista04 Questão 01.cpp
@@ -0,0 +1,32 @@
+//01. Faça um programa que leia uma string e a imprima na tela.     
+
+
+
+//Ennyson jeielly. If23.
+
+#include <iostream>
+
+#include <string>
+
+
+
+using namespace std;
+
+
+
+int main() {
+
+    string palavra;
+
+    cout << "Digite uma palavra: ";
+
+    getline(cin, palavra);
+
+
+
+    cout << "A palavra digitada foi: " << palavra << endl;
+
+
+
+    return 0;
+}
